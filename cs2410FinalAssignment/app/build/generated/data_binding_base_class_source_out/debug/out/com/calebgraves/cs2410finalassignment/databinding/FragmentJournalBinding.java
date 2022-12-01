79bf464a08bc689871a7993c18121403e64fa20a
calebtgraves <calebtgraves@gmail.com>
2022-11-30 19:44:18 -0700

    worked on the UI for the journal page

--- cs2410FinalAssignment/app/build/generated/data_binding_base_class_source_out/debug/out/com/calebgraves/cs2410finalassignment/databinding/FragmentJournalBinding.java
+++ cs2410FinalAssignment/app/build/generated/data_binding_base_class_source_out/debug/out/com/calebgraves/cs2410finalassignment/databinding/FragmentJournalBinding.java
@@ -4,25 +4,43 @@ package com.calebgraves.cs2410finalassignment.databinding;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewbinding.ViewBinding;
+import androidx.viewbinding.ViewBindings;
 import com.calebgraves.cs2410finalassignment.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import java.lang.NullPointerException;
 import java.lang.Override;
+import java.lang.String;
 
 public final class FragmentJournalBinding implements ViewBinding {
   @NonNull
-  private final FrameLayout rootView;
+  private final ConstraintLayout rootView;
 
-  private FragmentJournalBinding(@NonNull FrameLayout rootView) {
+  @NonNull
+  public final FloatingActionButton addJournalEntry;
+
+  @NonNull
+  public final ConstraintLayout journalLayout;
+
+  @NonNull
+  public final RecyclerView journalRecyclerView;
+
+  private FragmentJournalBinding(@NonNull ConstraintLayout rootView,
+      @NonNull FloatingActionButton addJournalEntry, @NonNull ConstraintLayout journalLayout,
+      @NonNull RecyclerView journalRecyclerView) {
     this.rootView = rootView;
+    this.addJournalEntry = addJournalEntry;
+    this.journalLayout = journalLayout;
+    this.journalRecyclerView = journalRecyclerView;
   }
 
   @Override
   @NonNull
-  public FrameLayout getRoot() {
+  public ConstraintLayout getRoot() {
     return rootView;
   }
 
@@ -43,10 +61,28 @@ public final class FragmentJournalBinding implements ViewBinding {
 
   @NonNull
   public static FragmentJournalBinding bind(@NonNull View rootView) {
-    if (rootView == null) {
-      throw new NullPointerException("rootView");
-    }
+    // The body of this method is generated in a way you would not otherwise write.
+    // This is done to optimize the compiled bytecode for size and performance.
+    int id;
+    missingId: {
+      id = R.id.addJournalEntry;
+      FloatingActionButton addJournalEntry = ViewBindings.findChildViewById(rootView, id);
+      if (addJournalEntry == null) {
+        break missingId;
+      }
 
-    return new FragmentJournalBinding((FrameLayout) rootView);
+      ConstraintLayout journalLayout = (ConstraintLayout) rootView;
+
+      id = R.id.journalRecyclerView;
+      RecyclerView journalRecyclerView = ViewBindings.findChildViewById(rootView, id);
+      if (journalRecyclerView == null) {
+        break missingId;
+      }
+
+      return new FragmentJournalBinding((ConstraintLayout) rootView, addJournalEntry, journalLayout,
+          journalRecyclerView);
+    }
+    String missingId = rootView.getResources().getResourceName(id);
+    throw new NullPointerException("Missing required view with ID: ".concat(missingId));
   }
 }
