9c9fc62da1cc4fe347a6bf11208d36c9be8de9cd
Zach Red <A02339170@aggies.usu.edu>
2022-12-05 14:04:29 -0700

    The rest of the Gradle build

---
+++ cs2410FinalAssignment/app/build/tmp/kapt3/stubs/debug/com/calebgraves/cs2410finalassignment/models/Goal.java
@@ -0,0 +1,142 @@
+package com.calebgraves.cs2410finalassignment.models;
+
+import java.lang.System;
+
+@androidx.room.Entity()
+@kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u00000\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\t\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0003\n\u0002\u0018\u0002\n\u0002\b\u001c\n\u0002\u0010\u000b\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0002\b\u0087\b\u0018\u00002\u00020\u0001B5\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0005\u0012\u0006\u0010\u0006\u001a\u00020\u0005\u0012\u0006\u0010\u0007\u001a\u00020\u0005\u0012\u0006\u0010\b\u001a\u00020\t\u0012\u0006\u0010\n\u001a\u00020\t\u00a2\u0006\u0002\u0010\u000bJ\t\u0010\u001e\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u001f\u001a\u00020\u0005H\u00c6\u0003J\t\u0010 \u001a\u00020\u0005H\u00c6\u0003J\t\u0010!\u001a\u00020\u0005H\u00c6\u0003J\t\u0010\"\u001a\u00020\tH\u00c6\u0003J\t\u0010#\u001a\u00020\tH\u00c6\u0003JE\u0010$\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00052\b\b\u0002\u0010\u0006\u001a\u00020\u00052\b\b\u0002\u0010\u0007\u001a\u00020\u00052\b\b\u0002\u0010\b\u001a\u00020\t2\b\b\u0002\u0010\n\u001a\u00020\tH\u00c6\u0001J\u0013\u0010%\u001a\u00020&2\b\u0010\'\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010(\u001a\u00020)H\u00d6\u0001J\t\u0010*\u001a\u00020\u0005H\u00d6\u0001R\u001e\u0010\u0006\u001a\u00020\u00058\u0006@\u0006X\u0087\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\f\u0010\r\"\u0004\b\u000e\u0010\u000fR\u001e\u0010\u0007\u001a\u00020\u00058\u0006@\u0006X\u0087\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0010\u0010\r\"\u0004\b\u0011\u0010\u000fR\u001e\u0010\n\u001a\u00020\t8\u0006@\u0006X\u0087\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0012\u0010\u0013\"\u0004\b\u0014\u0010\u0015R\u001e\u0010\u0002\u001a\u00020\u00038\u0006@\u0006X\u0087\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0016\u0010\u0017\"\u0004\b\u0018\u0010\u0019R\u001e\u0010\b\u001a\u00020\t8\u0006@\u0006X\u0087\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u001a\u0010\u0013\"\u0004\b\u001b\u0010\u0015R\u001e\u0010\u0004\u001a\u00020\u00058\u0006@\u0006X\u0087\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u001c\u0010\r\"\u0004\b\u001d\u0010\u000f\u00a8\u0006+"}, d2 = {"Lcom/calebgraves/cs2410finalassignment/models/Goal;", "", "id", "", "title", "", "category", "description", "startDate", "Ljava/util/Date;", "endDate", "(JLjava/lang/String;Ljava/lang/String;Ljava/lang/String;Ljava/util/Date;Ljava/util/Date;)V", "getCategory", "()Ljava/lang/String;", "setCategory", "(Ljava/lang/String;)V", "getDescription", "setDescription", "getEndDate", "()Ljava/util/Date;", "setEndDate", "(Ljava/util/Date;)V", "getId", "()J", "setId", "(J)V", "getStartDate", "setStartDate", "getTitle", "setTitle", "component1", "component2", "component3", "component4", "component5", "component6", "copy", "equals", "", "other", "hashCode", "", "toString", "app_debug"})
+public final class Goal {
+    @androidx.room.PrimaryKey(autoGenerate = true)
+    private long id;
+    @org.jetbrains.annotations.NotNull()
+    @androidx.room.ColumnInfo()
+    private java.lang.String title;
+    @org.jetbrains.annotations.NotNull()
+    @androidx.room.ColumnInfo()
+    private java.lang.String category;
+    @org.jetbrains.annotations.NotNull()
+    @androidx.room.ColumnInfo()
+    private java.lang.String description;
+    @org.jetbrains.annotations.NotNull()
+    @androidx.room.ColumnInfo()
+    private java.util.Date startDate;
+    @org.jetbrains.annotations.NotNull()
+    @androidx.room.ColumnInfo()
+    private java.util.Date endDate;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.calebgraves.cs2410finalassignment.models.Goal copy(long id, @org.jetbrains.annotations.NotNull()
+    java.lang.String title, @org.jetbrains.annotations.NotNull()
+    java.lang.String category, @org.jetbrains.annotations.NotNull()
+    java.lang.String description, @org.jetbrains.annotations.NotNull()
+    java.util.Date startDate, @org.jetbrains.annotations.NotNull()
+    java.util.Date endDate) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public boolean equals(@org.jetbrains.annotations.Nullable()
+    java.lang.Object other) {
+        return false;
+    }
+    
+    @java.lang.Override()
+    public int hashCode() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public java.lang.String toString() {
+        return null;
+    }
+    
+    public Goal(long id, @org.jetbrains.annotations.NotNull()
+    java.lang.String title, @org.jetbrains.annotations.NotNull()
+    java.lang.String category, @org.jetbrains.annotations.NotNull()
+    java.lang.String description, @org.jetbrains.annotations.NotNull()
+    java.util.Date startDate, @org.jetbrains.annotations.NotNull()
+    java.util.Date endDate) {
+        super();
+    }
+    
+    public final long component1() {
+        return 0L;
+    }
+    
+    public final long getId() {
+        return 0L;
+    }
+    
+    public final void setId(long p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component2() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getTitle() {
+        return null;
+    }
+    
+    public final void setTitle(@org.jetbrains.annotations.NotNull()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component3() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getCategory() {
+        return null;
+    }
+    
+    public final void setCategory(@org.jetbrains.annotations.NotNull()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component4() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getDescription() {
+        return null;
+    }
+    
+    public final void setDescription(@org.jetbrains.annotations.NotNull()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.Date component5() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.Date getStartDate() {
+        return null;
+    }
+    
+    public final void setStartDate(@org.jetbrains.annotations.NotNull()
+    java.util.Date p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.Date component6() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.Date getEndDate() {
+        return null;
+    }
+    
+    public final void setEndDate(@org.jetbrains.annotations.NotNull()
+    java.util.Date p0) {
+    }
+}
---
+++ cs2410FinalAssignment/app/build/tmp/kapt3/stubs/debug/com/calebgraves/cs2410finalassignment/models/JournalEntry.java
@@ -0,0 +1,104 @@
+package com.calebgraves.cs2410finalassignment.models;
+
+import java.lang.System;
+
+@androidx.room.Entity()
+@kotlin.Metadata(mv = {1, 7, 1}, k = 1, d1 = {"\u0000.\n\u0002\u0018\u0002\n\u0002\u0010\u0000\n\u0000\n\u0002\u0010\t\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0016\n\u0002\u0010\u000b\n\u0002\b\u0002\n\u0002\u0010\b\n\u0002\b\u0002\b\u0087\b\u0018\u00002\u00020\u0001B%\u0012\u0006\u0010\u0002\u001a\u00020\u0003\u0012\u0006\u0010\u0004\u001a\u00020\u0005\u0012\u0006\u0010\u0006\u001a\u00020\u0007\u0012\u0006\u0010\b\u001a\u00020\u0007\u00a2\u0006\u0002\u0010\tJ\t\u0010\u0018\u001a\u00020\u0003H\u00c6\u0003J\t\u0010\u0019\u001a\u00020\u0005H\u00c6\u0003J\t\u0010\u001a\u001a\u00020\u0007H\u00c6\u0003J\t\u0010\u001b\u001a\u00020\u0007H\u00c6\u0003J1\u0010\u001c\u001a\u00020\u00002\b\b\u0002\u0010\u0002\u001a\u00020\u00032\b\b\u0002\u0010\u0004\u001a\u00020\u00052\b\b\u0002\u0010\u0006\u001a\u00020\u00072\b\b\u0002\u0010\b\u001a\u00020\u0007H\u00c6\u0001J\u0013\u0010\u001d\u001a\u00020\u001e2\b\u0010\u001f\u001a\u0004\u0018\u00010\u0001H\u00d6\u0003J\t\u0010 \u001a\u00020!H\u00d6\u0001J\t\u0010\"\u001a\u00020\u0007H\u00d6\u0001R\u001e\u0010\b\u001a\u00020\u00078\u0006@\u0006X\u0087\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\n\u0010\u000b\"\u0004\b\f\u0010\rR\u001e\u0010\u0004\u001a\u00020\u00058\u0006@\u0006X\u0087\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u000e\u0010\u000f\"\u0004\b\u0010\u0010\u0011R\u001e\u0010\u0002\u001a\u00020\u00038\u0006@\u0006X\u0087\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0012\u0010\u0013\"\u0004\b\u0014\u0010\u0015R\u001e\u0010\u0006\u001a\u00020\u00078\u0006@\u0006X\u0087\u000e\u00a2\u0006\u000e\n\u0000\u001a\u0004\b\u0016\u0010\u000b\"\u0004\b\u0017\u0010\r\u00a8\u0006#"}, d2 = {"Lcom/calebgraves/cs2410finalassignment/models/JournalEntry;", "", "id", "", "date", "Ljava/util/Date;", "title", "", "content", "(JLjava/util/Date;Ljava/lang/String;Ljava/lang/String;)V", "getContent", "()Ljava/lang/String;", "setContent", "(Ljava/lang/String;)V", "getDate", "()Ljava/util/Date;", "setDate", "(Ljava/util/Date;)V", "getId", "()J", "setId", "(J)V", "getTitle", "setTitle", "component1", "component2", "component3", "component4", "copy", "equals", "", "other", "hashCode", "", "toString", "app_debug"})
+public final class JournalEntry {
+    @androidx.room.PrimaryKey(autoGenerate = true)
+    private long id;
+    @org.jetbrains.annotations.NotNull()
+    @androidx.room.ColumnInfo()
+    private java.util.Date date;
+    @org.jetbrains.annotations.NotNull()
+    @androidx.room.ColumnInfo()
+    private java.lang.String title;
+    @org.jetbrains.annotations.NotNull()
+    @androidx.room.ColumnInfo()
+    private java.lang.String content;
+    
+    @org.jetbrains.annotations.NotNull()
+    public final com.calebgraves.cs2410finalassignment.models.JournalEntry copy(long id, @org.jetbrains.annotations.NotNull()
+    java.util.Date date, @org.jetbrains.annotations.NotNull()
+    java.lang.String title, @org.jetbrains.annotations.NotNull()
+    java.lang.String content) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public boolean equals(@org.jetbrains.annotations.Nullable()
+    java.lang.Object other) {
+        return false;
+    }
+    
+    @java.lang.Override()
+    public int hashCode() {
+        return 0;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    @java.lang.Override()
+    public java.lang.String toString() {
+        return null;
+    }
+    
+    public JournalEntry(long id, @org.jetbrains.annotations.NotNull()
+    java.util.Date date, @org.jetbrains.annotations.NotNull()
+    java.lang.String title, @org.jetbrains.annotations.NotNull()
+    java.lang.String content) {
+        super();
+    }
+    
+    public final long component1() {
+        return 0L;
+    }
+    
+    public final long getId() {
+        return 0L;
+    }
+    
+    public final void setId(long p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.Date component2() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.util.Date getDate() {
+        return null;
+    }
+    
+    public final void setDate(@org.jetbrains.annotations.NotNull()
+    java.util.Date p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component3() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getTitle() {
+        return null;
+    }
+    
+    public final void setTitle(@org.jetbrains.annotations.NotNull()
+    java.lang.String p0) {
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String component4() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.NotNull()
+    public final java.lang.String getContent() {
+        return null;
+    }
+    
+    public final void setContent(@org.jetbrains.annotations.NotNull()
+    java.lang.String p0) {
+    }
+}
